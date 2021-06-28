8053c26a9dc837ec82e2e21775e51f8fdce851a8
Ritika941 <75617306+Ritika941@users.noreply.github.com>
2021-06-28 09:45:14 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Box from "./Box";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+    </div>
+  );
+}
+
+export default App;
---
+++ Box.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Box() {
+  return (
+    <div className="note">
+      <h1> React js and javascript </h1>
+      <p>
+        basics of web development using react.js and javascipt are coverded
+        along with a brief of html and css
+      </p>
+    </div>
+  );
+}
+export default Box;
